86f6e1844693fcb707487f74852e2f4246fab9c1
ziminas1990 <ziminas1990@gmail.com>
2019-01-16 20:59:26 +0300

    Application print only "Hellow, world!" string
    To prin version, just pass "-v" as first argument

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,14 @@
 #include "lib.h"
 #include <iostream>
+#include <string.h>
 
 int main (int argc, char *argv[])
 {
-    std::cout << "Hellow, world!\n" <<
-                 "Version: " << version_major() << "." << version_minor() <<
+  std::cout << "Hellow, world!" << std::endl;
+
+  if (argc == 2 && !strcmp(argv[1], "-v")) {
+    std::cout << "Version: " << version_major() << "." << version_minor() <<
                  ", build #" << version_patch() << std::endl;
-    return 0;
+  }
+  return 0;
 }
